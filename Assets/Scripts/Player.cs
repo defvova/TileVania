5bbf3d80c79b8e6986708dad4b81184f476d7463
Vova <volodimir.partytskyi@gmail.com>
2020-07-14 21:35:11 +0300

    Add movement horizontally to player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] private float acceleration = 712f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        Vector2 force = new Vector2(x, rb.velocity.y);
+
+        rb.AddForce(force * acceleration * Time.deltaTime);
+    }
+}

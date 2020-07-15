6c9efee1388cc59b932c12d84970ca5cb2b0e7a9
Vova <volodimir.partytskyi@gmail.com>
2020-07-15 14:03:00 +0300

    Add animation running state to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,29 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private float acceleration = 500f;
     [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
 
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private Animator animator;
     private Rigidbody2D rb;
     private bool facingRight;
     private float horizontalValue;
-    private Vector3 currentVelocity = Vector3.zero;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        Move();
-        Flip();
+        MovePlayer();
+        MoveSprite();
+        FlipSprite();
     }
 
-    private void Flip()
+    private void FlipSprite()
     {
         if ((horizontalValue < 0 && !facingRight) || (horizontalValue > 0 && facingRight))
         {
@@ -34,12 +38,26 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void Move()
+    private void MovePlayer()
     {
         horizontalValue = Input.GetAxisRaw("Horizontal");
-        float xSpeed = horizontalValue * acceleration * Time.fixedDeltaTime;
-        Vector3 targetVelocity = new Vector2(xSpeed, rb.velocity.y);
+        Vector3 targetVelocity = new Vector2(GetSpeed(), rb.velocity.y);
 
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref currentVelocity, movementSmoothing);
+    }
+
+    private void MoveSprite()
+    {
+        animator.SetBool("isRunning", IsRunning());
+    }
+
+    private float GetSpeed()
+    {
+        return horizontalValue * acceleration * Time.fixedDeltaTime;
+    }
+
+    private bool IsRunning()
+    {
+        return Mathf.Abs(GetSpeed()) > Mathf.Epsilon;
     }
 }
